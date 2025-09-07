02585ac311d6e5be56cf887abccdc6062b82443a
nikirich <nikstershmit@gmail.com>
2025-09-07 10:44:16 +0300

    Updated the overflow handling, added the use of trigonometrical functions in the Shunting Yard solving + more functions(log, sqrt)

--- core/evaluator.py
+++ core/evaluator.py
@@ -24,41 +24,41 @@ class Evaluator:
             'sqrt': math.sqrt,
         }
 
-def evaluate(self):
-    stack = []
+    def evaluate(self):
+        stack = []
 
-    for token in self.postfix_tokens:
-        if token.replace('.', '', 1).isdigit():
-            stack.append(float(token))
-        elif token in self.ops:
-            b = stack.pop()
-            a = stack.pop()
-            try:
-                result = self.ops[token](a, b)
-                result = self._handle_overflow(result)
-                stack.append(result)
-            except OverflowError:
-                stack.append(float('inf'))
-        elif token in self.functions:
-            a = stack.pop()
-            try:
-                result = self.functions[token](a)
-                result = self._handle_overflow(result)
-                stack.append(result)
-            except OverflowError:
-                stack.append(float('inf'))
-        else:  # variable support later
-            stack.append(token)
+        for token in self.postfix_tokens:
+            if token.replace('.', '', 1).isdigit():
+                stack.append(float(token))
+            elif token in self.ops:
+                b = stack.pop()
+                a = stack.pop()
+                try:
+                    result = self.ops[token](a, b)
+                    result = self._handle_overflow(result)
+                    stack.append(result)
+                except OverflowError:
+                    stack.append(float('inf'))
+            elif token in self.functions:
+                a = stack.pop()
+                try:
+                    result = self.functions[token](a)
+                    result = self._handle_overflow(result)
+                    stack.append(result)
+                except OverflowError:
+                    stack.append(float('inf'))
+            else:  # variable support later
+                stack.append(token)
 
-    result = stack[0] if stack else None
-    if isinstance(result, float):
-        return f"{result:.6g}"  # auto scientific notation if large/small
-    return result
+        result = stack[0] if stack else None
+        if isinstance(result, float):
+            return f"{result:.6g}"  # auto scientific notation if large/small
+        return result
 
-def _handle_overflow(self, num):
-    if abs(num) > 1e308:
-        return float('inf')
-    elif 0 < abs(num) < 1e-308:
-        return 0.0
-    return num
+    def _handle_overflow(self, num):
+        if abs(num) > 1e308:
+            return float('inf')
+        elif 0 < abs(num) < 1e-308:
+            return 0.0
+        return num
 
