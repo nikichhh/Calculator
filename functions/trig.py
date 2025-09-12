8c23fca1de55160b77ea2afa0c379ffad1da837d
nikirich <nikstershmit@gmail.com>
2025-09-12 17:02:21 +0300

    Add trigonometric, logarithmic, power, rounding, and statistical functions

--- functions/trig.py
+++ functions/trig.py
@@ -18,3 +18,39 @@ class Tan(Function):
     arg_spec = 1
     def evaluate(self, x): 
         return math.tan(x)
+    
+class ASin(Function):
+    name = "asin"
+    arg_spec = 1
+    def evaluate(self, x): 
+        return math.asin(x)
+
+class ACos(Function):
+    name = "acos"
+    arg_spec = 1
+    def evaluate(self, x): 
+        return math.acos(x)
+
+class ATan(Function):
+    name = "atan"
+    arg_spec = 1
+    def evaluate(self, x): 
+        return math.atan(x)
+    
+class Sinh(Function):
+    name = "sinh"
+    arg_spec = 1
+    def evaluate(self, x):
+        return math.sinh(x)
+
+class Cosh(Function):
+    name = "cosh"
+    arg_spec = 1
+    def evaluate(self, x):
+        return math.cosh(x)
+    
+class Tanh(Function):
+    name = "tanh"
+    arg_spec = 1
+    def evaluate(self, x): 
+        return math.tanh(x)
